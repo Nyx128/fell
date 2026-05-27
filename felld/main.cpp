8371c0e4899faca08ee71bfb3dbb453e542a31f7
Nyx <neelabhroghosh23@gmail.com>
2026-05-27 23:37:26 +0530

    fix felld ignore oopsie

---
+++ felld/main.cpp
@@ -0,0 +1,13 @@
+#include "broker/broker.hpp"
+#include <iostream>
+
+int main() {
+  try {
+    fell::Broker broker;
+    broker.run(7700);
+  } catch (const std::exception &e) {
+    std::cerr << "Fatal broker daemon error: " << e.what() << std::endl;
+    return 1;
+  }
+  return 0;
+}
